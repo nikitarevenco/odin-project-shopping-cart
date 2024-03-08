6ca397ffd0b3a0d38578619a24772e615eff570d
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-08 19:33:51 +0000

    feat: basic routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
 import "./styles/reset.css";
 import "./styles/theme.css";
 import "./styles/defaults.css";
 import "./styles/fonts.css";
+import Router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
