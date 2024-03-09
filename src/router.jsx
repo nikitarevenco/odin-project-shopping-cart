0df684af62c814123538c8805608e0c7b1a4c9cf
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-09 12:58:04 +0000

    feat: configure routing

--- src/router.jsx
+++ src/router.jsx
@@ -1,20 +1,41 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import ProductCategory from "./pages/ProductCategory/ProductCategory";
-import ProductItem from "./pages/ProductItem/ProductItem";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import categoryLoader from "./pages/App/categoryLoader";
+import itemLoader from "./pages/Item/itemLoader";
+import Category from "./pages/Item/Category";
+import App from "./pages/App/App";
+import Item from "./pages/ProductItem/ProductItem";
+import Register from "./pages/Register/Register";
+import FAQ from "./pages/FAQ/FAQ";
+import Contact from "./pages/Contact/Contact";
+import History from "./pages/History/History";
+import Cart from "./pages/Cart/Cart";
+import Checkout from "./pages/Checkout/Checkout";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import productLoader from "./pages/ProductItem/productLoader";
 
 function Router() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/:category",
-      element: <ProductCategory />,
-      children: [{ path: "/:category/:product", element: <h2>Hey!</h2> }],
-    },
-  ]);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<App />} loader={categoryLoader}>
+        <Route path=":category" element={<Category />} loader={itemLoader}>
+          <Route path=":item" element={<Item />} loader={productLoader} />
+        </Route>
+        <Route path="register" element={<Register />} />
+        <Route path="faq" element={<FAQ />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="history" element={<History />} />
+        <Route path="cart" element={<Cart />}>
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    )
+  );
 
   return <RouterProvider router={router} />;
 }
